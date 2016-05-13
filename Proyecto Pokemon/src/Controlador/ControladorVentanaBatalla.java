0916641837364253af84e1374b4c3eedd478e6ed
ErickxDD <erickrachell@gmail.com>
2016-05-13 14:05:31 -0600

    controlador ventana batalla

---
+++ Proyecto Pokemon/src/Controlador/ControladorVentanaBatalla.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+import Vista.VentanaBatalla;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+/**
+ *
+ * @author GersanElPuto
+ */
+public class ControladorVentanaBatalla implements ActionListener{
+    VentanaBatalla ventanaBatalla;
+    
+    public ControladorVenatanaBatalla(){
+        this.ventanaBatalla = ventanaBatalla;
+    }
+    
+    public void actionPerformed(ActionEvent e){
+        if(e.getActionCommand().equalsIgnoreCase("Batalla")){
+            
+        }
+             
+}
+    
+
+}
